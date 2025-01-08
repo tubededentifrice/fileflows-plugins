fa8f7407eecc6824edee9680e56be4951acd0a81
Vincent Courcelle <github.me@5en1.com>
2025-01-08 09:21:09 +0400

    Update and rename Max Kbps.js to MiB Per Hour.js

---
+++ Video/MiB Per Hour.js
@@ -0,0 +1,45 @@
+/**
+ * @description Check the size in MiB per hour of the input by checking the full file size and dividing by the video duration.
+ * @author Vincent Courcelle
+ * @param {int} MaxMiBPerHour MBytes per hour threshold (1024^2)
+ * @output Actual MB per hour below or equal the threshold
+ * @output Actual MB per hour above the threshold
+ * @output Unable to get video duration or other problem
+ */
+function Script(MaxMiBPerHour) {
+    const OUTPUT_BELOW = 1;
+    const OUTPUT_ABOVE = 2;
+    const OUTPUT_UNABLE = 3;
+
+    // Make extra sure it's actually an int
+    MaxMiBPerHour = parseInt(MaxMiBPerHour);
+
+    if (!MaxMiBPerHour) {
+        Logger.ILog("No MaxMiBPerHour was provided or is invalid number, failing: " + MaxMiBPerHour);
+        return OUTPUT_UNABLE;
+    }
+
+    const fileSize = Variables.file.Orig.Size;
+    const duration = Variables.video?.Duration;
+    if(!duration) {
+        Logger.ILog("No duration found");
+        return OUTPUT_UNABLE;
+    }
+
+    const mibPerHour = fileSize / duration * 3600 / 1024 / 1024;
+    Logger.ILog("File size: " + fileSize);
+    Logger.ILog("Duration: " + duration);
+    Logger.ILog("Detected mibPerHour: " + mibPerHour);
+
+    if (mibPerHour <= MaxMiBPerHour) {
+        Logger.ILog("Below threshold of " + MaxMiBPerHour);
+        return OUTPUT_BELOW;
+    }
+    if (mibPerHour > MaxMiBPerHour) {
+        Logger.ILog("Above threshold of " + MaxMiBPerHour);
+        return OUTPUT_ABOVE;
+    }
+
+    Logger.ILog("Could not compute MiB per hour");
+    return OUTPUT_UNABLE;
+}
