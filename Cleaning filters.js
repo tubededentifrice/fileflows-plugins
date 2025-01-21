632125ec857f797df033d9e2c8f922926a22a353
Vincent Courcelle <github.me@5en1.com>
2025-01-21 21:54:16 +0400

    Allowing to force a filter by passing variable

--- Cleaning filters.js
+++ Cleaning filters.js
@@ -7,6 +7,7 @@
 function Script() {
     const year = Variables.VideoMetadata?.Year || 2012;
     const genres = Variables.VideoMetadata?.Genres || [];
+    const hqdn3d = Variables.hqdn3d;
 
     const ffmpeg = Variables.FfmpegBuilderModel;
     if(!ffmpeg) {
@@ -28,23 +29,29 @@ function Script() {
 
     const filters = [];
 
-    // Checkout https://mattgadient.com/in-depth-look-at-de-noising-in-handbrake-with-imagevideo-examples/
-    if (year <= 1990) {
-        // At this age, even Animation needs denoising
-        filters.push('hqdn3d=2:2:6:6');
-    } else if (genres===null || !genres.includes("Animation")) {
-        // Cleanup depending on how old it is, newer movies doesn't have noise at all
-        if (year <= 2000) {
+    if (hqdn3d) {
+        Logger.ILog(`Received forced filter hqdn3d=${hqdn3d}`);
+        filters.push(`hqdn3d=${hqdn3d}`);
+    } else {
+        // Checkout https://mattgadient.com/in-depth-look-at-de-noising-in-handbrake-with-imagevideo-examples/
+        if (year <= 1990) {
+            // At this age, even Animation needs denoising
             filters.push('hqdn3d=2:2:6:6');
-        } else if (year <= 2010 || year === null) {
-            filters.push('hqdn3d=1:1:4:4');
-        } else if (year <= 2016) {
-            filters.push('hqdn3d=1:1:3:3');
-        } else {
-            // On more recent movies, just do a little bit of temporal denoising, just in case, should not be noticeable except for file size
-            filters.push('hqdn3d=0:0:3:3');
+        } else if (genres===null || !genres.includes("Animation")) {
+            // Cleanup depending on how old it is, newer movies doesn't have noise at all
+            if (year <= 2000) {
+                filters.push('hqdn3d=2:2:6:6');
+            } else if (year <= 2010 || year === null) {
+                filters.push('hqdn3d=1:1:4:4');
+            } else if (year <= 2016) {
+                filters.push('hqdn3d=1:1:3:3');
+            } else {
+                // On more recent movies, just do a little bit of temporal denoising, just in case, should not be noticeable except for file size
+                filters.push('hqdn3d=0:0:3:3');
+            }
         }
     }
+    
 
     // if (year <= 1990 || (year < 2016 && !genres.includes("Animation"))) {
     //     // filters.push('nlmeans=1.0:7:5:3:3'); // Very good results but super slow -- can't make the OpenCl or Vulkan versions work; probably not strong enough though
